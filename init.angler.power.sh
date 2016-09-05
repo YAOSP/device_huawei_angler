c2d50d6c7f1f727db20c3920f4e23553577541f8
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-09-05 22:17:12 +0200

    Revert "cpufreq: use ondemand instead of interactive"
    This reverts commit 7a9bab804337c77331b8af4d19e989b00147467e.

--- init.angler.power.sh
+++ init.angler.power.sh
@@ -43,46 +43,38 @@ write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
 # Limit A57 max freq from msm_perf module in case CPU 4 is offline
 write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000 6:960000 7:960000"
 
-# # configure governor settings for little cluster
-# write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
-# restorecon -R /sys/devices/system/cpu # must restore after interactive
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
-# write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
-# write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
-
-# We are using ondemand
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ondemand
-restorecon -R /sys/devices/system/cpu # must restore after ondemand
+# configure governor settings for little cluster
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
 
 # online CPU4
 write /sys/devices/system/cpu/cpu4/online 1
 
-# # configure governor settings for big cluster
-# write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
-# restorecon -R /sys/devices/system/cpu # must restore after interactive
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
-# write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-# write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
-
-# We are using ondemand
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor ondemand
-restorecon -R /sys/devices/system/cpu # must restore after ondemand
+# configure governor settings for big cluster
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 
 # restore A57's max
 copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
